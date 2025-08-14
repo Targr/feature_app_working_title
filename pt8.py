75eb9ff6c0a4058d65cbbf135cc9d6fc948864db
Targr <btarg888@gmail.com>
2025-08-14 15:03:25 -0500

    Update pt8.py

--- pt8.py
+++ pt8.py
@@ -299,5 +299,7 @@ def api_batch():
         "zip_base64": zip_b64
     })
 
-
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
 
